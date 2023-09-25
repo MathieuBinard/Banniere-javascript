e67a02b568a372511b9d6de68f0ce9639365fe7a
MathieuBinard <93312097+MathieuBinard@users.noreply.github.com>
2023-09-25 18:28:58 +0200

    Add files via upload

---
+++ Mini projet Banière/script.js
@@ -0,0 +1,17 @@
+console.log('connecté'); 
+
+document.querySelector('.btn-succes');
+//je séléctionne et je stocke
+//bouton .btn-success;
+const btnSuccess = document.querySelector('.btn-success');
+console.log(btnSuccess);
+
+//DIV cookies;
+const cookies = document.querySelector('.cookies');
+console.log(cookies);
+
+btnSuccess.addEventListener('click', function(){
+    console.log('bouton cliqué !');
+    cookies.style.opacity = "0";
+});
+
